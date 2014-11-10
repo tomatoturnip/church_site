007e9228b979188087b8a385374bfdf467d05f46
Lydia Ho <lyd.w.ho@gmail.com>
2014-11-10 14:57:07 -0500

    Add navbar

---
+++ app/assets/javascripts/navbar.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    function toggleNavbarMethod() {
+        if ($(window).width() > 768) {
+            $('.navbar .dropdown').on('mouseover', function(){
+                $('.dropdown-toggle', this).trigger('click');
+            }).on('mouseout', function(){
+                $('.dropdown-toggle', this).trigger('click').blur();
+            });
+        }
+        else {
+            $('.navbar .dropdown').off('mouseover').off('mouseout');
+        }
+    }
+
+     // toggle navbar hover
+        toggleNavbarMethod();
+
+        // bind resize event
+        $(window).resize(toggleNavbarMethod);
+});
