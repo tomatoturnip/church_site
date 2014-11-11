fbce82089c4ebeadf50565c20f6055f6b881fae5
Lydia Ho <lyd.w.ho@gmail.com>
2014-11-11 13:23:15 -0500

    Fix modal login

--- app/assets/javascripts/login.js
+++ app/assets/javascripts/login.js
@@ -1,8 +1,3 @@
-$('.login_link').click(function(){
-
-    $('.modal-body').load('/render/62805',function(result){
-      $('#myModal').modal({show:true});
-  });
-
-
+$(function(){
+  $("#loginmodal").modal({show:false });
 });
