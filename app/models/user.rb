8bb0aa186070ae3a4c7c1f9f027c754d21176fa2
Lydia Ho <lyd.w.ho@gmail.com>
2014-11-11 01:08:25 -0500

    Clean up code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable, :omniauthable,
+  devise :database_authenticatable, :registerable, :omniauthable,
          :recoverable, :rememberable, :trackable, :validatable
 
   def full_name
