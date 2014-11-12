3f209cbbc028f63b8401dc0e8fe077bcd1fc98c3
Lydia Ho <lyd.w.ho@gmail.com>
2014-11-12 17:13:12 -0500

    Comment out validation in Identity

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -2,7 +2,7 @@ class Identity < ActiveRecord::Base
   belongs_to :user
 
   validates :uid, :provider, presence: true
-  validates :uid, scope: :provider, uniqueness:true
+  # validates :uid, scope: :provider, uniqueness:true
 
   def self.find_for_oauth(auth)
     find_or_create_by(uid: auth.uid, provider: auth.provider)
